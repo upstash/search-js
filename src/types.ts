b43a36b28c14daf38b308963093b91296b2b8fae
CahidArda <cahidardaooz@hotmail.com>
2025-05-30 12:25:47 +0300

    feat: make metadata optional

--- src/types.ts
+++ src/types.ts
@@ -6,16 +6,16 @@ export type CommandParameters<TNonFieldsParams, TIndexMetadata> = keyof TIndexMe
   ? TNonFieldsParams & { metadata?: never }
   : TNonFieldsParams & { metadata: TIndexMetadata };
 
-export type UpsertParameters<TContent extends Dict, TIndexMetadata extends Dict> = CommandParameters<
+export type UpsertParameters<TContent extends Dict, TIndexMetadata extends object> = CommandParameters<
   { id: string; content: TContent },
   TIndexMetadata
 >;
 
-export type Document<TContent extends Dict, TMetadata extends Dict, TWithScore extends boolean = false> = {
+export type Document<TContent extends Dict, TMetadata extends object, TWithScore extends boolean = false> = {
   id: string;
   content: TContent;
   metadata?: TMetadata;
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 } & (TWithScore extends true ? { score: number } : {});
 
-export type SearchResult<TContent extends Dict, TMetadata extends Dict> = Document<TContent, TMetadata, true>[]
+export type SearchResult<TContent extends Dict, TMetadata extends object> = Document<TContent, TMetadata, true>[]
