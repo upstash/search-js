e18b4697822667f2490ba530e814c9b973e61df8
CahidArda <cahidardaooz@hotmail.com>
2025-06-10 19:44:43 +0300

    fix: suspense issue
    https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- examples/nextjs-movies/src/app/layout.tsx
+++ examples/nextjs-movies/src/app/layout.tsx
@@ -2,12 +2,10 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { Modal, Input, Switch } from "antd";
-import Link from "next/link";
 import '@ant-design/v5-patch-for-react-19';
-import { upsertData } from "./actions";
+import { SearchBar } from "./search-bar";
+import { Suspense } from "react";
+import { Spin } from "antd";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,38 +22,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [query, setQuery] = useState("");
-  const [reranking, setReranking] = useState(false);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const router = useRouter();
-  const [isLocalhost, setIsLocalhost] = useState(false);
 
-
-  const searchParams = useSearchParams();
-  const sarchQuery = searchParams.get("query");
-
-  useEffect(() => {
-    if (typeof sarchQuery === "string") {
-      setQuery(sarchQuery);
-    }
-  }, [sarchQuery, setQuery])
-
-  useEffect(() => {
-    if (typeof window !== "undefined" && window.location.hostname === "localhost") {
-      setIsLocalhost(true);
-    }
-  }, []);
-
-  const toggleReranking = (checked: boolean) => {
-    if (checked) {
-      setIsModalVisible(true);
-    }
-    setReranking(checked);
-  };
-
-  const handleSubmit = () => {
-    router.push(`/?query=${query}&reranking=${reranking}`);
-  };
 
   return (
     <html lang="en">
@@ -63,72 +30,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="min-h-screen bg-emerald-50 flex flex-col">
-          <header className="bg-emerald-700 text-white p-4 flex justify-center">
-            <div className="w-full max-w-4xl flex justify-between items-center">
-              <Link href="/" className="text-lg font-semibold text-white cursor-pointer" onClick={() => {
-                setQuery("")}}>
-                Home
-              </Link>
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  handleSubmit();
-                }}
-                className="flex items-center gap-4 text-base"
-              >
-                <Input
-                  placeholder="Search..."
-                  value={query}
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                    setQuery(e.target.value)
-                  }
-                  className="w-64"
-                />
-                <Switch
-                  checked={reranking}
-                  onChange={toggleReranking}
-                  className="bg-emerald-500 cursor-pointer"
-                />
-                Reranking
-                <button
-                  type="submit"
-                  disabled={!query}
-                  className="bg-emerald-500 px-4 rounded-md hover:bg-emerald-600 cursor-pointer h-8 transition-colors"
-                >
-                  Search
-                </button>
-              </form>
-              {isLocalhost ? (
-                <button
-                  onClick={async () => {
-                    await upsertData();
-                  }}
-                  className="bg-red-500 px-4 rounded-md hover:bg-red-600 cursor-pointer h-8 transition-colors"
-                >
-                  Upsert Data
-                </button>
-              ) : (
-                <div className="w-0" />
-              )}
-            </div>
-          </header>
-          <main className="flex-1 p-4">
-            {children}
-          </main>
-          <Modal
-            title="Warning"
-            open={isModalVisible}
-            onOk={() => setIsModalVisible(false)}
-            onCancel={() => setIsModalVisible(false)}
-          >
-            <p>Enabling reranking is expensive. Please proceed with caution.</p>
-            <Link
-              href="https://upstash.com/pricing/search"
-              className="text-sm underline"
-            >
-              Pricing
-            </Link>
-          </Modal>
+          <Suspense fallback={<div className="flex items-center justify-center h-screen"><Spin size="large" /></div>}>
+            <SearchBar />
+            <main className="flex-1 p-4">
+              {children}
+            </main>
+          </Suspense>
         </div>
       </body>
     </html>
