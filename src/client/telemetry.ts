19bb6c88445d2fe88ca34d814a0f201ff18aa49b
CahidArda <cahidardaooz@hotmail.com>
2025-07-15 14:30:59 +0300

    fix: rm duplicate typeof

--- src/client/telemetry.ts
+++ src/client/telemetry.ts
@@ -4,14 +4,14 @@ export function getRuntime() {
   if (typeof process === "object" && typeof process.versions == "object" && process.versions.bun)
     return `bun@${process.versions.bun}`;
 
-  // @ts-expect-error EdgeRuntime not recognized
-  if (typeof typeof EdgeRuntime === "string") {
-    return "edge-light";
-  }
-
   if (typeof process === "object" && typeof process.version === "string") {
     return `node@${process.version}`;
   }
 
-  return "Undetermined";
+  // @ts-expect-error EdgeRuntime not recognized
+  if (typeof EdgeRuntime === "string") {
+    return "edge-light";
+  }
+
+  return "undetermined";
 }
