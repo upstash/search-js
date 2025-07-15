de4703ab1f4cbd7d4229c3b1cebb356f8777784a
josh <hello@joshtriedcoding.com>
2025-07-15 14:05:46 +0300

    fix: telemetry-caused crash in undetermined runtimes

--- src/client/telemetry.ts
+++ src/client/telemetry.ts
@@ -4,6 +4,14 @@ export function getRuntime() {
   if (typeof process === "object" && typeof process.versions == "object" && process.versions.bun)
     return `bun@${process.versions.bun}`;
 
-  // @ts-expect-error Silence compiler
-  return typeof EdgeRuntime === "string" ? "edge-light" : `node@${process.version}`;
+  // @ts-expect-error EdgeRuntime not recognized
+  if (typeof typeof EdgeRuntime === "string") {
+    return "edge-light";
+  }
+
+  if (typeof process === "object" && typeof process.version === "string") {
+    return `node@${process.version}`;
+  }
+
+  return "Undetermined";
 }
